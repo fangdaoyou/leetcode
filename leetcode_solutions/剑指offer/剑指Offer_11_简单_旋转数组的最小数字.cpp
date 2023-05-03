cc6cf801d402a0d5c8c4b53d4c991a83ae2623c0
“方道友” <471727250@qq.com>
2023-05-03 20:21:04 +0800

    initial

---
+++ leetcode_solutions/剑指offer/剑指Offer_11_简单_旋转数组的最小数字.cpp
@@ -0,0 +1,23 @@
+//
+// Created by fang on 2023/5/3.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    int minArray(vector<int>& numbers) {
+        int left = 0, right = numbers.size()-1;
+        while (left < right){
+            int mid = left + (right - left) / 2;
+            if (numbers[mid] < numbers[right]) {
+                right = mid;
+            } else if (numbers[mid] > numbers[right]) {
+                left = mid+1;
+            } else right--;
+        }
+        return numbers[left];
+    }
+};
