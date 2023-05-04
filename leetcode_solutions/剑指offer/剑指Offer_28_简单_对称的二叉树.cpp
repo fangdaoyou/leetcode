f98b456ef1698e99edb603fe4943b763e2b8abb5
fangdaoyou <471727250@qq.com>
2023-05-04 21:50:18 +0800

    initial

--- leetcode_solutions/剑指offer/剑指Offer_28_简单_对称的二叉树.cpp
+++ leetcode_solutions/剑指offer/剑指Offer_28_简单_对称的二叉树.cpp
@@ -15,11 +15,12 @@ struct TreeNode {
 
 class Solution {
 public:
-    bool dfs(TreeNode *left, TreeNode *right){
+    bool dfs(TreeNode *left, TreeNode *right) {
         if (left == nullptr && right == nullptr) return true;
         if (left == nullptr || right == nullptr) return false;
         return left->val == right->val && dfs(left->left, right->right) && dfs(left->right, right->left);
     }
+
     bool isSymmetric(TreeNode *root) {
         if (root == nullptr) return true;
         return dfs(root->left, root->right);
