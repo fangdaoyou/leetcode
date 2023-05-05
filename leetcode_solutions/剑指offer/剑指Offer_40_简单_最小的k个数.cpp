fc57a27af3daa00c6464353908705d86fa87020a
fangdaoyou <471727250@qq.com>
2023-05-05 22:23:35 +0800

    initial

--- leetcode_solutions/剑指offer/剑指Offer_40_简单_最小的k个数.cpp
+++ leetcode_solutions/剑指offer/剑指Offer_40_简单_最小的k个数.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> getLeastNumbers(vector<int>& arr, int k) {
+    vector<int> getLeastNumbers(vector<int> &arr, int k) {
         vector<int> ans(k, 0);
         if (k == 0) {
             return ans;
@@ -18,7 +18,7 @@ public:
         for (int i = 0; i < k; ++i) {
             pq.push(arr[i]);
         }
-        for (int i = k; i < (int)arr.size(); ++i) {
+        for (int i = k; i < (int) arr.size(); ++i) {
             if (pq.top() > arr[i]) {
                 pq.pop();
                 pq.push(arr[i]);
--- leetcode_solutions/剑指offer/剑指Offer_42_简单_连续子数组的最大和.cpp
+++ leetcode_solutions/剑指offer/剑指Offer_42_简单_连续子数组的最大和.cpp
@@ -9,11 +9,11 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int maxSubArray(vector<int> &nums) {
         int n = nums.size(), ans = nums[0];
         for (int i = 1; i < n; ++i) {
-            if (nums[i-1] >= 0){
-                nums[i] += nums[i-1];
+            if (nums[i - 1] >= 0) {
+                nums[i] += nums[i - 1];
             }
             ans = max(ans, nums[i]);
         }
