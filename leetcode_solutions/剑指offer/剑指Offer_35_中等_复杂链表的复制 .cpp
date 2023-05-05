9f78f5eb5e3860a84c0b3306ae037e8b86a4861d
fangdaoyou <471727250@qq.com>
2023-05-05 15:11:58 +0800

    initial

---
+++ leetcode_solutions/剑指offer/剑指Offer_35_中等_复杂链表的复制 .cpp
@@ -0,0 +1,47 @@
+//
+// Created by fang on 2023/5/5.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if (head == nullptr) return head;
+        Node* newHead = new Node(head->val), * cur = newHead, * oriHead = head;
+        unordered_map<Node*, Node*> map;
+        map[head] = newHead;
+        while (head->next != nullptr){
+            cur->next = new Node(head->next->val);
+            cur = cur->next;
+            head = head->next;
+
+            map[head] = cur;
+        }
+        cur = newHead;
+        while (cur != nullptr){
+            cur->random = map[oriHead->random];
+            cur = cur->next;
+            oriHead = oriHead->next;
+        }
+        return newHead;
+    }
+};
