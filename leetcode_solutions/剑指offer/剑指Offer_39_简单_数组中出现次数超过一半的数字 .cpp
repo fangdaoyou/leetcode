163517332a718f83fdbba7587756cc994e255699
fangdaoyou <471727250@qq.com>
2023-05-05 22:26:06 +0800

    initial

--- leetcode_solutions/剑指offer/剑指Offer_39_简单_数组中出现次数超过一半的数字 .cpp
+++ leetcode_solutions/剑指offer/剑指Offer_39_简单_数组中出现次数超过一半的数字 .cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    int majorityElement(vector<int> &nums) {
         int count = 1;
         int num = nums[0];
         for (int i = 1; i < nums.size(); ++i) {
             if (num == nums[i]) count++;
             else count--;
 
-            if (count == 0){
+            if (count == 0) {
                 num = nums[i];
                 count = 1;
             }
