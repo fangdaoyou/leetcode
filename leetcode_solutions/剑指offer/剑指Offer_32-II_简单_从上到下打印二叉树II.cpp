a22fa0ac59a2332a4884979c2e9bd077c9c0019b
fangdaoyou <471727250@qq.com>
2023-05-05 10:45:54 +0800

    initial

---
+++ leetcode_solutions/剑指offer/剑指Offer_32-II_简单_从上到下打印二叉树II.cpp
@@ -0,0 +1,43 @@
+//
+// Created by fang on 2023/5/5.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+//  Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        deque<TreeNode*> deque1;
+        if (root == nullptr) return ans;
+        deque1.push_back(root);
+
+        while (!deque1.empty()){
+            int deq_size = deque1.size();
+            vector<int> temp;
+            for (int i = 0; i < deq_size; ++i) {
+                TreeNode *node = deque1.front();
+                deque1.pop_front();
+                temp.push_back(node->val);
+                if (node->left != nullptr)
+                    deque1.push_back(node->left);
+                if (node->right != nullptr)
+                    deque1.push_back(node->right);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
