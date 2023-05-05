61551f8d9320d954a9d2ea852b4f78247dc9fb3c
fangdaoyou <471727250@qq.com>
2023-05-05 10:35:18 +0800

    initial

---
+++ leetcode_solutions/剑指offer/剑指_Offer_31_中等_栈的压入弹出序列.cpp
@@ -0,0 +1,30 @@
+//
+// Created by fang on 2023/5/5.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        int n = pushed.size();
+        if(n == 0) return true;
+        stack<int> s;
+        int index = 0;
+        for(int i = 0; i < n; i++)
+        {
+            s.push(pushed[i]);
+            while(s.top() == popped[index])
+            {
+                s.pop();
+                index++;
+                if(s.empty()){
+                    break;
+                }
+            }
+        }
+        return index == n;
+    }
+};
