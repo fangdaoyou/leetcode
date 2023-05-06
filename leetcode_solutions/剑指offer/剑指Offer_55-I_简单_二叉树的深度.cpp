df355a6bd14ae704bbd8f16e6a1a8fd9c179d1be
fangdaoyou <471727250@qq.com>
2023-05-06 19:40:25 +0800

    initial

--- leetcode_solutions/剑指offer/剑指Offer_55-I_简单_二叉树的深度.cpp
+++ leetcode_solutions/剑指offer/剑指Offer_55-I_简单_二叉树的深度.cpp
@@ -20,6 +20,6 @@ class Solution {
 public:
     int maxDepth(TreeNode *root) {
         if (root == nullptr) return 0;
-        return maxDepth(root->left, root->right) + 1;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
